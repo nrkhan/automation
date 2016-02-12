92c00d795b2852e87bd913279f829d2f34d12529
nrkhan <s.qanaveed@gmail.com>
2016-02-12 10:37:01 +0500

    Removed

--- tests/acceptance/ForgetPasswordCept.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('Check Forgot password Validation with Wrong Email');
-$I->amOnPage('http://172.16.3.15:9090/mids/auth/login/');
-$I->click('Forgot Your Password?');
-$I->fillField('email','hsgasgd@jhgsdh.com');
-$I->click(['class' => 'btn-primary']);
-$I->see('find a usersdda with that e-mail address.')
-?>
